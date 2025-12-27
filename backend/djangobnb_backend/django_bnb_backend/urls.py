5b7a4c20af9adb1628ec6e97529be0d455ebd9a1
Binod Bist <thebinodbist@gmail.com>
2025-12-27 19:45:36 +0545

    docker set up

--- backend/djangobnb_backend/django_bnb_backend/urls.py
+++ backend/djangobnb_backend/django_bnb_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangobnb_backend project.
+URL configuration for django_bnb_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
